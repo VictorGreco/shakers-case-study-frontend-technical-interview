719a5aa1f4a1423433ffb81bbaeb435f71e0caaa
VictorGreco <victorgrecotech@gmail.com>
2025-12-11 23:38:09 +0100

    fix: header

--- src/app/projects/_components/MenuIcon/MenuIcon.tsx
+++ src/app/projects/_components/MenuIcon/MenuIcon.tsx
@@ -1,11 +1,7 @@
-import SvgIcon from '@mui/material/SvgIcon';
-
 export default function MenuIcon() {
     return (
-        <SvgIcon inheritViewBox>
             <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M2.66675 4H13.3334M2.66675 8H13.3334M2.66675 12H13.3334" stroke="#033028" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
             </svg>
-        </SvgIcon>
     )
 }
